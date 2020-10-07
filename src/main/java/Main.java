281adb1f64947bbfa8bab16585fd0bcdeab6655f
dobrienUNCG <d_obrien@uncg.edu>
2020-10-07 16:34:34 -0400

    Example Upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+ 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test.red();
+
+	}
+
+	
+	
+
+}
