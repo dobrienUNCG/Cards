eeb059ceff55c71b275c3aa24118812639f81a98
dobrienUNCG <d_obrien@uncg.edu>
2020-10-19 20:51:38 -0400

    Dev Environment Setup Part 1

---
+++ app/src/main/java/Cards/app/JavaFXController.java
@@ -0,0 +1,32 @@
+package Cards.app;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+import javafx.fxml.FXMLLoader;
+
+import java.net.URL;
+
+public class JavaFXController extends Application{
+    @Override
+    public void start(Stage stage) throws Exception{
+
+
+        Parent root = FXMLLoader.load(getClass().getResource("/Menu.fxml"));
+        stage.setScene(new Scene((root)));
+        /*
+        Long Notice:
+            I found out that you cannot actually have a scene (or is it a stage?)
+            within the FXML file or it will fail to load with a weird error stating
+            that stage cannot be cast to parent.
+         */
+        stage.show();
+    }
+    public static void show(){
+        launch();
+    }
+
+
+}
